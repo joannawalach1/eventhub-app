e9356e72cb11b02b9ee61d324b64886891782caf
joannawalach1 <joanna.walach@op.pl>
2026-02-01 15:07:11 +0100

    add_basic_structure

---
+++ eventHub-backend/src/main/java/com/next/EventHub_backend/EventHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.next.EventHub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventHubBackendApplication.class, args);
+	}
+
+}
---
+++ eventHub-backend/src/test/java/com/next/EventHub_backend/EventHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.EventHub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
